ed836794989de780a7a9ed74051e8730ce9d5431
Esteban Licea <esolo@Estebans-MacBook-Pro.local>
2020-04-17 13:21:04 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import PhotoContainer from './PhotoContainer'
+
+import Axios from "axios";
+
+
+
+
 
 function App() {
+  const [date, setDate] = useState([])
+
+  useEffect(()=>{
+    Axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=ZUTQNJgHM8K7HjVcadIEkVh6UD1UnScImjfb0S1S')
+    
+    .then(response => {
+      console.log(response.data);
+      setDate(response.data);
+    
+    })
+    .catch(error => console.log("Error!", error))
+  },[])
+  
+    
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+  app! Have fun {date.copyright}!
       </p>
+      <PhotoContainer/>
     </div>
   );
 }
---
+++ src/PhotoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PhotoContainer ( props ){
+    return (
+        <div>
+            <h2>Authosr</h2>
+            <img src = {props.hdurl} alt='t'></img>
+
+            
+        </div>
+    )
+}
